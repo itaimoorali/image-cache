0add67f994450c9e69abb825bec8916dfef0ce3a
taimoorali736 <taimur.ali@blueeast.com>
2018-02-20 17:31:52 +0500

    Added the restrict in directive

--- src/app/common/directives/on-image-error.directive.js
+++ src/app/common/directives/on-image-error.directive.js
@@ -1,5 +1,6 @@
 imageCacheApp.directive('onErrorSrc', function() {
     return {
+        restrict: 'A',
         link: function(scope, element, attrs) {
             element.bind('error', function() {
                 element.remove()
